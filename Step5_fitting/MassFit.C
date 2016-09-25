6f1b7850b697ea441c624ad969bd5029e7ac2126
Ana Trisovic <ana.trisovic@gmail.com>
2016-09-25 14:25:38 +0200

    org

--- Step5_fitting/MassFit.C
+++ Step5_fitting/MassFit.C
@@ -1,5 +1,6 @@
-#include "RootHeaders.h"
-#include "RooFitHeaders.h"
+#include "/var/clus/usera/cliff/PostDoc/Header_Files/RootHeaders.h"
+#include "/var/clus/usera/cliff/PostDoc/Header_Files/RooFitHeaders.h"
+#include "/var/clus/usera/cliff/PostDoc/Header_Files/Global_Parameters.h"
 #include "iostream"
 #include "fstream"
 
@@ -8,66 +9,65 @@ using namespace TMath;
 using namespace std;
 
 // Get the data
-TFile f1("D2hMuMu12_MagU_2PiMuMuOS_NTuple_Reduced.root","read");
-//../Step3_MC/MC_D2PiMuMu12_MagDown_NTuples_fin.root","read"); 
-//
+TFile f1("/var/pcfst/r21/lhcb/cliff/ntuples/2011_Data/BdToKstarMuMu_MagAll_Data_1.03fb_BDT_presel_clean.root","read");
 
-void Fit_D2Pimumu_Mass( RooDataSet* Data, RooRealVar* D_MM, Double_t MassMin, Double_t MassMax, RooRealVar *FitResults[2],  TFile &fOutput, TString Title = "") {
+void Fit_Kstarmumu_Mass( RooDataSet* Data, RooRealVar* B0_M, Double_t MassMin, Double_t MassMax, Double_t Fit_Results[11], Double_t Fit_Result_Errors[10], TString Param_Names[11],  TPaveText* Label, TFile &fOutput, TString Title = "") {
 
   // Create the PDFs
 
   Int_t nFitParams = 9;
   Double_t nCands = Data->sumEntries();
 
-  // D Double Crystal Ball
-  // D mass mean 1871
-  // From PDG D 1869
-  // PDG D_s mass 1968.3
-  RooRealVar mean_D("m_{D}", "m_{D}", 1869.6, 1850.0, 1890.0, "MeV/c^{2}");
-  RooRealVar mean_Ds("m_{Ds}", "m_{Ds}", 1968.3, 1950.0, 1990.0, "MeV/c^{2}");
-  RooRealVar sigma1("#sigma_{1}", "#sigma_{1}", 27.7, 10.0, 40.0, "MeV/c^{2}");
-  RooRealVar sigma2("#sigma_{2}", "#sigma_{2}", 30.0, 10.0, 40.0, "MeV/c^{2}"); 
-  RooRealVar alpha("#alpha", "#alpha", 2.020,.1,10.); 
-  RooRealVar n("n", "n", 1.824,0.,10.);
+
+  // B0 Double Crystal Ball
+  RooRealVar mean_B0("m_{B^{0}}", "m_{B^{0}}", Global_Bd0_Mass, 5260.0, 5310.0, "MeV/c^{2}");
+  RooRealVar sigma1("#sigma_{1}", "#sigma_{1}", 17.0, 10.0, 25.0, "MeV/c^{2}");
+  RooRealVar sigma2("#sigma_{2}", "#sigma_{2}", 45.0, 25.0, 70.0, "MeV/c^{2}"); //26.0, 100.0, "MeV/c^{2}");
+  RooRealVar alpha("#alpha", "#alpha", 2.24735); //1.4, 1.0, 3.0);//0.0, 10.0);
+  //RooRealVar n("n", "n", 3.0, 0.5, 25.0);
+  //nFitParams++;
+  RooRealVar n("n", "n", 0.934334);//3.66);
   
   RooRealVar frac1("f_{1}", "f_{1}", 0.83, 0.0, 1.0);
-  //D_CB
-  RooCBShape D_CB("D_CB", "D_CB", *D_MM, mean_D, sigma1, alpha, n); 
-  RooCBShape Ds_CB("Ds_CB", "Ds_CB", *D_MM, mean_Ds, sigma2, alpha, n); 
+  
+  RooCBShape B0_CB1("B0_CB1", "B0_CB1", *B0_M, mean_B0, sigma1, alpha, n); 
+  RooCBShape B0_CB2("B0_CB2", "B0_CB2", *B0_M, mean_B0, sigma2, alpha, n); 
 
-  //RooAddPdf D_PDF("D_PDF", "D_PDF", RooArgList(D_CB1, D_CB2), RooArgList(frac1));
+  RooAddPdf B0_PDF("B0_PDF", "B0_PDF", RooArgList(B0_CB1, B0_CB2), RooArgList(frac1));
 
-  // Ds
-  //RooConstVar deltaMBsBd("#Delta m_{s-d}", "#Delta m_{s-d}", Global_Delta_M_Bs_Bd);
-  //RooFormulaVar mean_Ds( "m_{D_{s}}","m_{D_{s}}",RooArgList( mean_D, deltaMBsBd ));
+  // Bs0
+  RooConstVar deltaMBsBd("#Delta m_{s-d}", "#Delta m_{s-d}", Global_Delta_M_Bs_Bd);
+  RooFormulaVar mean_Bs0( "m_{B^{0}_{s}}","m_{B^{0}_{s}}","@0 + @1",RooArgList( mean_B0, deltaMBsBd ));
    
- // RooCBShape Ds_CB1("Ds_CB1", "Ds_CB1", *D_MM, mean_Ds, sigma1, alpha, n); 
- // RooCBShape Ds_CB2("Ds_CB2", "Ds_CB2", *D_MM, mean_Ds, sigma2, alpha, n); 
+  RooCBShape Bs0_CB1("Bs0_CB1", "Bs0_CB1", *B0_M, mean_Bs0, sigma1, alpha, n); 
+  RooCBShape Bs0_CB2("Bs0_CB2", "Bs0_CB2", *B0_M, mean_Bs0, sigma2, alpha, n); 
 
-  RooAddPdf D_Ds_PDF("Ds_PDF", "Ds_PDF", RooArgList(D_CB, Ds_CB), RooArgList(frac1));
+  RooAddPdf Bs0_PDF("Bs0_PDF", "Bs0_PDF", RooArgList(Bs0_CB1, Bs0_CB2), RooArgList(frac1));
+
+  // Make a gaussian constraint on the fraction between the two CBs
+  RooRealVar BdBsFrac("BdBsFrac", "fraction between Bs and Bd", 0.007, 0.0, 0.02);
+  RooRealVar BdBsFracMean("BdBsFracMean", "mean of fraction between Bs and Bd", 0.0070);
+  RooRealVar BdBsFracSigma("BdBsFracSigma", "mean of fraction between Bs and Bd", 0.0020);
+  RooGaussian BdBsFracCons( "BdBsFracCons", "constraint on the fraction between Bs and Bd", BdBsFrac, BdBsFracMean, BdBsFracSigma);
 
   // Combinatorial Background
   RooRealVar K_CombBG("K_{CombBG}", "K_{CombBG}", -0.006, -0.1, 0.1, "c^{2}/MeV");//-1.0, 0.0, "c^{2}/MeV");
-  RooExponential CombBG_PDF("CombBG_PDF", "CombBG_PDF", *D_MM, K_CombBG);
+  RooExponential CombBG_PDF("CombBG_PDF", "CombBG_PDF", *B0_M, K_CombBG);
  
-  //Ds, D+, BG
   // Yields
-  RooRealVar D_Yield("N_D", "N_{D}", 0.10*nCands, 0.0, 2.0*nCands);
-  RooRealVar CombBG_Yield("N_D Bg", "N_{D Bg}", 0.90*nCands, 0.0, 2.0*nCands);
+  RooRealVar B0_Yield("N_{B^{0}}", "N_{B^0}", 0.94*nCands, 0.0, 2.0*nCands);
+  RooRealVar CombBG_Yield("N_{B^{0} Bg}", "N_{B^0 Bg}", 0.05*nCands, 0.0, 2.0*nCands);
+  RooFormulaVar Bs0_Yield("N_{B^{0}_{s}}", "N_{B^{0}_{s}}", "@0*@1", RooArgList(B0_Yield, BdBsFrac));
 
   // Total PDFs
- RooAddPdf PDF("PDF", "PDF", RooArgList(D_Ds_PDF, CombBG_PDF), RooArgSet(D_Yield, CombBG_Yield ));
+  RooAddPdf PDF("PDF", "PDF", RooArgList(B0_PDF, CombBG_PDF, Bs0_PDF ), RooArgSet(B0_Yield, CombBG_Yield, Bs0_Yield ));
  
   // Perform the fit
-  // PDF
-  PDF.fitTo(*Data, Extended(1), RooFit::Save( true ), PrintEvalErrors(-1), Minos(kTRUE) );
-
-  FitResults[0]=&D_Yield;
-  FitResults[1]=&CombBG_Yield;
+  PDF.fitTo(*Data, Extended(1), RooFit::Save( true ), RooFit::ExternalConstraints( RooArgSet(BdBsFracCons) ), PrintEvalErrors(-1), Minos(kTRUE) );
 
   // Plot the results
   TCanvas cMass(Title + " Mass Fit", Title + " Mass Fit", 1);
-  RooPlot* pMass = D_MM->frame(Bins(50)) ;
+  RooPlot* pMass = B0_M->frame(Bins(50)) ;
   pMass->GetXaxis()->SetLabelOffset(0.015);
   pMass->GetXaxis()->SetLabelSize(0.05);
   pMass->GetXaxis()->SetTitleSize(0.05);
@@ -78,21 +78,20 @@ void Fit_D2Pimumu_Mass( RooDataSet* Data, RooRealVar* D_MM, Double_t MassMin, Do
   pMass->SetTitle("");
 
   Data->plotOn(pMass,DataError(RooAbsData::SumW2) ) ;
-  //PDF
   PDF.plotOn(pMass) ;
   RooHist* hMass_Pull = pMass->pullHist(); 
   Double_t Fit_chi2 = pMass->chiSquare(nFitParams);
-  PDF.plotOn(pMass, Components(D_CB), LineColor(kRed));
-  PDF.plotOn(pMass, Components(Ds_CB), LineColor(kOrange-3));
+  PDF.plotOn(pMass, Components(B0_CB1), LineColor(kRed));
+  PDF.plotOn(pMass, Components(B0_CB2), LineColor(kOrange-3));
   PDF.plotOn(pMass, Components(CombBG_PDF), LineColor(kGray+2)) ;
-  PDF.plotOn(pMass, Components(D_Ds_PDF), LineColor(kViolet)) ;
+  PDF.plotOn(pMass, Components(Bs0_PDF), LineColor(kViolet)) ;
   pMass->Draw();
-  //Label->Draw("SAME");
+  Label->Draw("SAME");
   cMass.Write();
 
   // Construct a histogram with the residuals of the data w.r.t. the curve
   TCanvas cMass_Pull(Title + " Mass Fit Pull", Title + " Mass Fit Pull", 1);
-  RooPlot* pMass_Pull = D_MM->frame() ;
+  RooPlot* pMass_Pull = B0_M->frame() ;
   pMass_Pull->GetXaxis()->SetLabelOffset(0.015);
   pMass_Pull->GetXaxis()->SetLabelSize(0.05);
   pMass_Pull->GetXaxis()->SetTitleSize(0.05);
@@ -108,37 +107,48 @@ void Fit_D2Pimumu_Mass( RooDataSet* Data, RooRealVar* D_MM, Double_t MassMin, Do
 
 }
 
-
+// Do something!
 void Main() 
 {
+ 
   cout << "Hello there" << endl;
   
   // Limits
-  Double_t MassMin = 1800.0;
-  Double_t MassMax = 2050.0;
+  Double_t MassMin = 5150.0;
+  Double_t MassMax = 5800.0;
 
   // Get the tree
-  TTree* D2PimumuTree = (TTree*) f1.Get("D2PimumuOSTuple/DecayTree"); 
+  TTree* KstarmumuTree = (TTree*) f1.Get("treee"); 
 
   // Create the dataset variables
-  RooRealVar* D_MM = new RooRealVar("D_MM", "D_MM", MassMin, MassMax, "MeV/c^{2}");
-  RooRealVar* BDT = new RooRealVar("BDT", "BDT", 0.1, 0.25);
+  RooRealVar* B0_M = new RooRealVar("B0_M", "m_{J/#PsiK*^{0}}", MassMin, MassMax, "MeV/c^{2}");
+  RooRealVar* K_ProbNNp = new RooRealVar("K_ProbNNp", "K_ProbNNp", 0.0, 1.0);
+  RooRealVar* K_ProbNNk = new RooRealVar("K_ProbNNk", "K_ProbNNk", 0.0, 1.0);
+  RooRealVar* Pi_ProbNNp = new RooRealVar("Pi_ProbNNp", "Pi_ProbNNp", 0.0, 1.0);
+  RooRealVar* Pi_ProbNNpi = new RooRealVar("Pi_ProbNNpi", "Pi_ProbNNpi", 0.0, 1.0);
+ 
 
   // Create the RooArgSet that holds the variables
-  RooArgSet D2PimumuSet(*D_MM, *BDT);
-  RooDataSet *All_Data = new RooDataSet("All_Data", "All_Data", D2PimumuSet, Import(*D2PimumuTree));
+  RooArgSet KstarmumuSet(*B0_M, *K_ProbNNp, *K_ProbNNk, *Pi_ProbNNp, *Pi_ProbNNpi);
+  RooDataSet *All_Data = new RooDataSet("All_Data", "All_Data", KstarmumuSet, Import(*KstarmumuTree), Cut("(K_ProbNNk>K_ProbNNp)&&(Pi_ProbNNpi>Pi_ProbNNp)"));
    
+
   // Create an output file
-  TFile fOutput("Results/Output_MagUp.root","recreate");   
+  TFile fOutput("Results/Output.root","recreate");   
 
   // Create things to hold the fit results
-  RooRealVar *FitResults[2];
+  Double_t All_Fit_Results[11];
+  Double_t All_Fit_Result_Errors[10];
+  TString All_Param_Names[11];
   
-  // Perform the fit
-  Fit_D2Pimumu_Mass( All_Data, D_MM, MassMin, MassMax, FitResults, fOutput, "All" );
  
+
+  // Perform the fit
+  Fit_Kstarmumu_Mass( All_Data, B0_M, MassMin, MassMax, All_Fit_Results, All_Fit_Result_Errors, All_Param_Names, lhcb7TeVMCR, fOutput, "All" );
+ 
+
   ofstream myfile;
-  myfile.open("Results/Output_MagUP_D2PimumuOSTuple.txt",ios::out | ios::trunc);
+  myfile.open("Results/Output_" + SProtonCut + "_" + SVersion + ".txt",ios::out | ios::trunc);
   myfile << "Results" << endl;
   myfile << "_______" << endl;
   myfile << "MassMin = " << MassMin << " MeV/c2" << endl;
@@ -146,9 +156,10 @@ void Main()
   myfile << endl;
   myfile << "All Fit Results" << endl;
   myfile << "________________________________________________________" << endl;
-  
-  myfile << "D Yield (N_D)" << " = " << FitResults[0]->getValV() << " +/- " << FitResults[0].getError() << endl;
-  myfile << "Comb background Yield (N_D BG)" << " = " << FitResults[1]->getValV() << " +/- " << FitResults[1].getError() << endl;
+  for( Int_t i = 0; i < 10; i++){
+    myfile << All_Param_Names[i] << " = " << All_Fit_Results[i] << " +/- " << All_Fit_Result_Errors[i] << endl;
+  }
+  myfile << All_Param_Names[10] << " = " << All_Fit_Results[10] << endl;
   myfile.close();
 
 } // Do something!
--- Step5_fitting/addBDT.C
+++ Step5_fitting/addBDT.C
@@ -7,17 +7,32 @@
 
 #include "TMVA/Reader.h"
 
-void addBDT()
+void help()
 {
-  TString fname="../Ntuples/D2hMuMu12_MagD_2PiMuMuOS_NTuple_Reduced.root";
+  cout << endl << "      addbdt       " << endl
+       << " -h             : print this help " << endl
+       << " -f [file name]    " << endl
+       << " -d [dir name]  :  directory inside root file " << endl
+       << " -t [tree name] :  tree inside directory   " << endl
+       << " -c [category]  : 0 = B_s0 muplus muminus " << endl
+       << "                : 1 = B0 muplus muminus   " << endl
+       << "                : -1 = B_s0 muplus muplus0   " << endl
+       << endl;
+  
+}
+
+void addBDT(int flaggo)
+{
+  TString fname="Ntuples/D2hMuMu12_MagU_2PiMuMuOS_NTuple_Reduced.root";
   TString dname="D2PimumuOSTuple";
   TString tname="DecayTree";
   Info("addBDT", "Going to add BDT for file  %s \n and tree %s/%s ", 
        fname.Data(), dname.Data(), tname.Data());
 
   //Load the file with the ntuple to update
-
-  TFile *f = TFile::Open(fname,"update");
+  TString met = "";
+  if(fname.Contains("castor")) met = "rfio:";
+  TFile *f = TFile::Open(met+fname,"update");
   f->cd(dname);
 
   TTree *t = (TTree*)f->Get(dname+"/"+tname);
@@ -25,112 +40,107 @@ void addBDT()
 
   //Declare variables and set branches
  
-  Double_t D_dira_ownpv;
-  Double_t D_ipchi2_ownpv;
-  Double_t D_endver_chi2;
-  Double_t d_tau;
-  Double_t d_mm;
+  Double_t B_s0_PT;
+  Double_t B_s0_eta;
+  Double_t B_s0_doca;
+  Double_t B_s0_cosnk;
+  Double_t muplus_PT;
+  Double_t muminus_PT;
+  Double_t B_s0_M;
+  Double_t B_s0_TAU;
 
-  Double_t piplus_pt;
-  Double_t piplus_ipchi2_ownpv;
-  Double_t piplus_p;
+  TBranch        *b_B_s0_PT;   //!
+  TBranch        *b_B_s0_eta;     //!
+  TBranch        *b_B_s0_cosnk;   //!
+  TBranch        *b_B_s0_doca;   //!
+  TBranch        *b_muminus_PT;   //!
+  TBranch        *b_muplus_PT;   //!
+  TBranch        *b_B_s0_M;     //!
+  TBranch        *b_B_s0_TAU;   //!
 
-  Double_t muplus_p;
-  Double_t muplus_pidmu;
+  TString Bname = "";
+  TString nameplus = "";
+  TString nameminus = "";
+  if (flaggo == 0){
+    Bname = "B_s0";
+    nameplus = "muplus";
+    nameminus = "muminus";
+  }else if (flaggo == 1){
+    Bname = "B0";
+    nameplus = "muplus";
+    nameminus = "muminus";
+  }else if (flaggo == 2){
+    Bname = "Bplus";
+    nameplus = "muplus";
+    nameminus = "muminus";
+  }else if (flaggo == 3){
+    Bname = "B_s0";
+    nameplus = "eplus";
+    nameminus = "muminus";
+  }else if (flaggo == 4){
+    Bname = "B_s0";
+    nameplus = "eplus";
+    nameminus = "eminus";
+  }else{
+    std::cout << "wrong flag" << std::endl;
+    return ;
+  }
 
-  Double_t muminus_p;
-  Double_t muminus_pidmu;
+  /*
+  factory->AddVariable( "D_dira := D_DIRA_OWNPV", 'F' );
+   factory->AddVariable( "D_ipchi := D_IPCHI2_OWNPV", 'F' );
+   factory->AddVariable( "D_edchi := D_ENDVERTEX_CHI2", 'F' );
+   factory->AddVariable( "pi_pt := piplus_PT", 'F' );
+   
+   factory->AddVariable( "pi_ipchi := piplus_IPCHI2_OWNPV", 'F' );
+   factory->AddVariable( "pi_p := piplus_P", 'F' );
+
+   factory->AddVariable( "max_mu_p := max(muminus_P, muplus_P)", 'F' );
+   factory->AddVariable( "min_mu_p := min(muminus_P, muplus_P)", 'F' );
+   factory->AddVariable( "max_mu_pt := max(muminus_PT, muplus_PT)", 'F' );
+   factory->AddVariable( "min_mu_pt := min(muminus_PT, muplus_PT)", 'F' );
+   factory->AddVariable( "D_tau := D_TAU", 'F' );
+  */
   
-  TBranch        *b_D_dira_ownpv;   //!
-  TBranch        *b_D_ipchi2_ownpv;     //!
-  TBranch        *b_D_endver_chi2;   //!
-  TBranch        *b_d_tau;     //!
-  TBranch        *b_d_mm;   //!
+  t->SetBranchAddress(Bname+"_PT", &B_s0_PT, &b_B_s0_PT);
+  t->SetBranchAddress(Bname+"_eta", &B_s0_eta, &b_B_s0_eta);
+  t->SetBranchAddress(Bname+"_doca", &B_s0_doca, &b_B_s0_doca);
+  t->SetBranchAddress(Bname+"_cosnk", &B_s0_cosnk, &b_B_s0_cosnk);
+  t->SetBranchAddress(nameplus+"_PT", &muplus_PT, &b_muplus_PT);
+  t->SetBranchAddress(nameminus+"_PT", &muminus_PT, &b_muminus_PT);
+  t->SetBranchAddress(Bname+"_M", &B_s0_M, &b_B_s0_M);
+  t->SetBranchAddress(Bname+"_TAU", &B_s0_TAU, &b_B_s0_TAU);
 
-  TBranch        *b_piplus_pt;   //!
-  TBranch        *b_piplus_p;   //!
-  TBranch        *b_piplus_ipchi2_ownpv;   //!
+  Float_t BDTD4;     //BDTS classifier output
+  Float_t var0,var1,var2,var3,var4,var5, var6, var7, var8, var9, var10, spect0,spect1, spect2;
 
-  TBranch        *b_muplus_p;   //!
-  TBranch        *b_muplus_pidmu;     //!
-
-  TBranch        *b_muminus_p;     //!
-  TBranch        *b_muminus_pidmu;   //!  
-
-  t->SetBranchAddress("D_DIRA_OWNPV", &D_dira_ownpv, &b_D_dira_ownpv);
-  t->SetBranchAddress("D_IPCHI2_OWNPV", &D_ipchi2_ownpv, &b_D_ipchi2_ownpv);
-  t->SetBranchAddress("D_ENDVERTEX_CHI2", &D_endver_chi2, &b_D_endver_chi2);
-  t->SetBranchAddress("D_TAU", &d_tau, &b_d_tau);
-  t->SetBranchAddress("D_MM", &d_mm, &b_d_mm);
-
-  t->SetBranchAddress("piplus_PT", &piplus_pt, &b_piplus_pt);
-  t->SetBranchAddress("piplus_IPCHI2_OWNPV", &piplus_ipchi2_ownpv, &b_piplus_ipchi2_ownpv);
-  t->SetBranchAddress("piplus_P", &piplus_p, &b_piplus_p);
-  t->SetBranchAddress("muminus_P", &muminus_p, &b_muminus_p);
-
-  t->SetBranchAddress("muplus_P", &muplus_p, &b_muplus_p);
-  t->SetBranchAddress("muplus_PIDmu", &muplus_pidmu, &b_muplus_pidmu);
-
-  t->SetBranchAddress("muminus_P", &muminus_p, &b_muminus_p);
-  t->SetBranchAddress("muminus_PIDmu", &muminus_pidmu, &b_muminus_pidmu);
-
-  Float_t BDT;     //BDTS classifier output
-  Float_t var0,var1,var2,var3,var4,var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, spect0,spect1, spect2;
-
-  TString method = "BDT";
+  TString method = "BDTD";
   std::cout << "----------------------------------------------------" << std::endl;
   std::cout << "\nMethod used: " << method << std::endl;
   std::cout << "----------------------------------------------------" << std::endl;
 
-  TBranch * BDTBranch = t->Branch("BDT",&BDT,"BDT/F");
+  TBranch * BDTD4Branch = t->Branch("BDTD4",&BDTD4,"BDTD4/F");
   
   TMVA::Reader *reader = new TMVA::Reader( "!Color:!Silent" );
-  reader->AddVariable("D_DIRA_OWNPV", &var0);
-  reader->AddVariable("D_IPCHI2_OWNPV", &var1);
-  reader->AddVariable("D_ENDVERTEX_CHI2", &var2);
-  reader->AddVariable("piplus_PT", &var3);
+  reader->AddVariable("D_DIRA_OWNPV",             &var0);
+  reader->AddVariable("D_IPCHI2_OWNPV",            &var1);
+  reader->AddVariable("D_ENDVERTEX_CHI2",           &var2);
+  reader->AddVariable("piplus_PT",          &var3);
   reader->AddVariable("piplus_IPCHI2_OWNPV", &var4);
   reader->AddVariable("piplus_P", &var5);
   reader->AddVariable("max(muminus_P, muplus_P)", &var6);
   reader->AddVariable("min(muminus_P, muplus_P)", &var7);
   reader->AddVariable("max(muminus_PT, muplus_PT)", &var8);
   reader->AddVariable("min(muminus_PT, muplus_PT)", &var9);
-
   reader->AddVariable("D_TAU", &var10);
-  reader->AddVariable("min(muminus_IPCHI2_OWNPV,muplus_IPCHI2_OWNPV)", &var11);
-  reader->AddVariable("max(muminus_IPCHI2_OWNPV,muplus_IPCHI2_OWNPV)", &var12);
-  
-  reader->AddVariable("D_PT", &var13);
-  reader->AddVariable("D_FDCHI2_OWNPV", &var14);
-  reader->AddVariable("D_P", &var15);
-/*
   reader->AddSpectator("D_MM",&spect0);
   reader->AddSpectator("muplus_PIDmu",&spect1);
   reader->AddSpectator("muminus_PIDmu",&spect2);
-  */
-/*
-    "0" Expression="D_DIRA_OWNPV" Label="D_dira" Title="D_dira" 
-    "1" Expression="D_IPCHI2_OWNPV" Label="D_ipchi" Title="D_ipchi" 
-    "2" Expression="D_ENDVERTEX_CHI2" Label="D_edchi" Title="D_edchi" 
-    "3" Expression="piplus_PT" Label="pi_pt" 
-    "4" Expression="piplus_IPCHI2_OWNPV" Label="pi_ipchi" 
-    "5" Expression="piplus_P" Label="pi_p" 
-    "6" Expression="max(muminus_P,muplus_P)" Label="max_mu_p" 
-    "7" Expression="min(muminus_P,muplus_P)" Label="min_mu_p" 
-    "8" Expression="max(muminus_PT,muplus_PT)" Label="max_mu_pt" 
-    "9" Expression="min(muminus_PT,muplus_PT)" Label="min_mu_pt" 
-    "10" Expression="D_TAU" Label="D_tau" 
-    "11" Expression="min(muminus_IPCHI2_OWNPV,muplus_IPCHI2_OWNPV)" Label="min_mu_ip" 
-    "12" Expression="max(muminus_IPCHI2_OWNPV,muplus_IPCHI2_OWNPV)" Label="max_mu_ip" 
-    "13" Expression="D_PT" Label="D_pt" 
-    "14" Expression="D_FDCHI2_OWNPV" Label="D_fd" 
-    "15" Expression="D_P" Label="D_p" 
-*/
-
+  
   //Ana: OK
   TString methodName = method;
   methodName += TString(" method"); 
-  TString weightFile = "../Step4_TMVA/weights/TMVAClassification_BDT.weights.xml"; 
+  TString weightFile = "weights/TMVAClassification_BDT.weights.xml"; 
 
   reader->BookMVA( methodName, weightFile );
 
@@ -141,17 +151,17 @@ void addBDT()
     t->GetEntry(i);
   
     //    var0 = B_s0_CTAU_ps;     
-    var0 = D_dira_ownpv;     
-    var1 = D_ipchi2_ownpv;    
-    var2 = D_endver_chi2;
-    var3 = piplus_pt;      
-    var4 = min(piplus_ipchi2_ownpv,piplus_p);  
-    var5 = max(piplus_ipchi2_ownpv,piplus_p);  
-    spect0 = muminus_p;
-    spect1 = muplus_p;
+    var0 = B_s0_PT;     
+    var1 = B_s0_eta;    
+    var2 = B_s0_doca;
+    var3 = B_s0_cosnk;      
+    var4 = min(muplus_PT,muminus_PT);  
+    var5 = max(muplus_PT,muminus_PT);  
+    spect0 = B_s0_M;
+    spect1 = B_s0_TAU;
 
-    BDT = reader->EvaluateMVA(methodName);
-    BDTBranch->Fill();
+    BDTD4 = reader->EvaluateMVA(methodName);
+    BDTD4Branch->Fill();
     
   }
   t->Write("",TObject::kOverwrite);
@@ -159,4 +169,78 @@ void addBDT()
     
   cout << endl << "-----------------------------------" << endl;
   
+}
+
+
+
+
+//============================================================
+// MAIN
+//============================================================
+int main(int argc, char **argv){
+  TString fname;
+  TString dname;
+  TString tname = "DecayTree";
+  int flaggo = 0;
+
+  if(argc<2){
+    help() ;
+    return 1;
+  }
+
+  for(Int_t i=1; i<argc; i++){
+    TString opt(argv[i]);
+    cout << " Parsing option: " << opt << endl;
+    if(opt.Contains("-h")){
+      help();
+      return 0;
+    }else if(opt.Contains("-f")){
+      i++;
+      TString val(argv[i]);
+      fname = val; 
+    }else if(opt.Contains("-d")){
+      i++;
+      TString val(argv[i]);
+      dname = val;
+    }else if(opt.Contains("-t")){
+      i++;
+      TString val(argv[i]);
+      tname = val;
+    }else if(opt.Contains("-c")){
+      i++;
+      TString val(argv[i]);
+      flaggo = val.Atoi();
+    }else{
+      Error("Option parsing...","unknown option %s",opt.Data());
+      return -1;
+    }
+  }
+
+  if(fname ==""){
+    Error("","You must specify the file name");
+    cout << endl;
+    help();
+    return -1;
+  }
+  
+
+  if(dname != ""){
+    addBDT(fname, dname, tname,flaggo);
+    return 0;
+  }
+  Info("" ," No directory specified: will try B2MuMuTuple and B2HHTuple" );
+  
+  
+  // If no dir is specified do both
+  addBDT(fname, "B2MuMuTuple", tname,  flaggo);
+  
+  cout << endl;
+  
+  
+  addBDT(fname, "B2HHTuple",   tname,  flaggo);
+
+
+  cout << " This is the end..." << endl;
+  
+  return 0;
 }

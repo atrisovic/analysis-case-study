c0746a3c49a1079353b4d868c0ae473a06a7b304
Ana Trisovic <ana.trisovic@gmail.com>
2016-12-02 18:09:36 +0100

    fix bugs with catalogs

--- Step1_getdata/MagDown-11/DaVinci_Options.py
+++ Step1_getdata/MagDown-11/DaVinci_Options.py
@@ -8,8 +8,6 @@ FileCatalog().Catalogs = [ "xmlcatalog_file:/afs/cern.ch/work/a/atrisovi/analysi
 
 from Configurables import DaVinci
 
-# ANA - these three lines are the locations of the candidates produced by three different stripping lines. You should modify these to include four(?) lines we discussed yesterday. You will need to find the exact name of the stripping lines from the stripping code.
-
 # Stripping 21r1
 KMuMuOSlocation = 'Phys/D2XMuMu_KOSLine/Particles' 
 KMuMuSSlocation = 'Phys/D2XMuMu_KSSLine/Particles'
@@ -18,7 +16,6 @@ PiMuMuSSlocation = 'Phys/D2XMuMu_PiSSLine/Particles'
 
 # Source: http://lhcb-release-area.web.cern.ch/LHCb-release-area/DOC/stripping/config/stripping21r1/index.html#charm
 
-# ANA - this line (below) is needed if you are running over MicroDSTs, which you will be if you are looking at stripping 21. You will need to change it to something like rootInTes = "/Event/Charm" (I guess your data is tin the charm stream, but it will be obvious from the book-keeping. Ask me if you get stuck!
 rootInTes = "/Event/Charm"
 
 #----------------------------------------
@@ -51,7 +48,6 @@ tuple.ToolList += [
     
     ]
 
-# Configure the TupleToolTISTOS (this controls how much trigger information goes into the ntuple)
 tuple.addTool(TupleToolTISTOS("TupleToolTISTOS"))
 tuple.TupleToolTISTOS.OutputLevel = 3
 tuple.TupleToolTISTOS.VerboseL0 = 1
@@ -59,9 +55,8 @@ tuple.TupleToolTISTOS.VerboseHlt1 = 1
 tuple.TupleToolTISTOS.VerboseHlt2 = 1
 tuple.TupleToolTISTOS.TriggerList=[
 
-# ANA - you need to change the list of triggers below to the ones that your decay modes come through. Look at Ed Greening's thesis - it should specify which triggers are important. They are all named with the format "<TriggerLevel><TriggerName>Decision" where TriggerLevel = L0, Hlt1, or Hlt2 and the TriggerName is the name of the specific trigger line e.g. "DiMuonLowMass" in "Hlt1DiMuonLowMassDecision"
-
-#page 51 (and 97 for e)
+# the list of triggers 
+# page 51 (and 97 for e)
 
   'L0DiMuonDecision'
     ,'L0MuonDecision'
@@ -91,8 +86,6 @@ tuple.TupleToolTISTOS.TriggerList=[
     ,'Hlt2CharmSemilepD2HMuMuWideMassDecision'
   ]
 
-
-# Tuple to get the LoKi versions of variables (leave this as it is, it's quite useful)
 LoKi_Vars = LoKi__Hybrid__TupleTool("LoKi_Vars")
 LoKi_Vars.Variables =  {
 
@@ -103,19 +96,8 @@ LoKi_Vars.Variables =  {
 
     }
 
-
-# ANA - these three chunks of code create the objects that make the ntuples. You will need to change each line to match your trigger line.
-
-# Bs2KKLTUBTuple = tuple.clone("D2KmumuOSTuple") # You can rename the tuple tool to something more appropriate e.g. 'D2KmumuOSTuple'
-# Bs2KKLTUBTuple.Inputs = [ KKlocation ] # This should match the locations you create at the top of the code
-# Bs2KKLTUBTuple.Decay = "B_s0 -> ^K+ ^K-" # This is the 'decay descriptor'. You will need to change it for each stripping line. You can find the correct form of the decay descriptors in the stripping code - they are different for each line. Probably something like "[D+ -> K+ mu+ mu-]cc" but check!
-
 # "[D+ -> K+ mu+ mu-]cc"
 # "[D- -> K+ mu- mu-]cc"
-
-#Bs2KKLTUBTuple.Branches = { "B_s0" :  "B_s0 -> K+ K-"}
-#Bs2KKLTUBTuple.addTool(TupleToolDecay, name="B_s0")
-#Bs2KKLTUBTuple.P2PVInputLocations = ["Phys/Bs2MuMusBs2KKLTUBLine/Particle2VertexRelations"]
 
 D2KmumuOSTuple = tuple.clone("D2KmumuOSTuple") #updated
 D2KmumuOSTuple.Inputs = [ KMuMuOSlocation ] #updated
@@ -153,24 +135,22 @@ D2PimumuSSTuple.P2PVInputLocations = ["Phys/D2XMuMu_PiSSLine/Particle2VertexRela
 # 2) Run within DaVinci                                                 
 #----------------------------------------
 
-magPol = "Down" # This is the magnet polarity of the data - there will be magnet up and magnet down sample in the book-keeping. Change this when you run over different datasets
-year = "11" # This specifies the year the data was taken in. It should match the data set you are running over.
+magPol = "Down" 
+year = "11" 
 
 DaVinci().RootInTES = rootInTes
-DaVinci().TupleFile     = "Stripping21r1_D2HMuMu"+year+"_Mag"+magPol+"_NTuples.root" # This is the name of the ntuple file that gets produced. You should change this to something more appropriate. It automatically includes the year and magnet polarity in the name.
+DaVinci().TupleFile     = "Stripping21r1_D2HMuMu"+year+"_Mag"+magPol+"_NTuples.root" 
 
-DaVinci().EvtMax                 = -1 # This specifies how many events to run over in each data file. Set this to a low number for quick tests (e.g. 1000). -1 means that you run over all the data.
-DaVinci().DataType               = "20"+year # The year the data was taken
-DaVinci().Simulation             = False # Whether or not this is simulated data. In this case, False is correct.
+DaVinci().EvtMax                 = -1 
+DaVinci().DataType               = "20"+year 
+DaVinci().Simulation             = False 
 
-# updated
-DaVinci().UserAlgorithms = [ D2KmumuOSTuple, D2KmumuSSTuple, D2PimumuOSTuple, D2PimumuSSTuple ]  # This is where you add the ntuple algorithms that you created earlier. You should have four, one for each stripping line.
+DaVinci().UserAlgorithms = [ D2KmumuOSTuple, D2KmumuSSTuple, D2PimumuOSTuple, D2PimumuSSTuple ]  
 
 # All OK bellow this point
-DaVinci().InputType = "MDST" # This is the type of file - in this case MDST which stands for MicroDST
+DaVinci().InputType = "MDST" 
 
 # Database tags
-# ANA - these are the database tags. There are two, the DDDBtag and the CondDBtag. These must match the database tags used to create the data, otherwise you'll get the wrong results. When you save the list of datafiles as a .py file from the book-keeping the database tags are automatically saved at the top of the file so you can copy them from there. Make sure to check they match each time you change data files. They vary between mag up and mag down and years.
 if year == "12":
     DaVinci().DDDBtag   = 'dddb-20130929-1' 
 #12 Mag Down Strip 21 dddb-20130929-1
--- Step1_getdata/MagDown-11/Ganga_Options.py
+++ Step1_getdata/MagDown-11/Ganga_Options.py
@@ -1,24 +1,22 @@
-# ANA - this is the file that you actually submit to Ganga. It controls the job, the output files and so on. You have to specify in this file the location of the DaVinci options that you want to run. In Ganga you run this file using the command: execfile("Ganga_Options_for_Ana.py"). 
-myApplication = DaVinci(version='v36r1p2') 					#OK
-myApplication.user_release_area  = "/afs/cern.ch/work/a/atrisovi/anastudy/MagDown-11"		#Maybe
-myApplication.platform           = "x86_64-slc6-gcc48-opt"			#Probably OK
+myApplication = DaVinci(version='v36r1p2') 					
+myApplication.user_release_area  = "/afs/cern.ch/work/a/atrisovi/analysis-case-study/Step1_getdata/MagDown-11"
+myApplication.platform           = "x86_64-slc6-gcc48-opt"			
 
-magPol = "Down" # Make sure the magPol and year match what you specify in your DaVinci options
+magPol = "Down" 
 year = "11"
-
+path = "/afs/cern.ch/work/a/atrisovi/analysis-case-study/Step1_getdata/MagDown-11/"
 fileName = "LHCbCollision11Beam3500GeV-VeloClosed-MagDownRealDataReco14Stripping21r190000000CHARMMDST.py"
 
 j = Job( application = myApplication )
-j.name = "S21"+magPol+year+"data" # This is just the name of the job in Ganga. You can change this to whatever you like.
-j.application.optsfile = [ File ( '/afs/cern.ch/work/a/atrisovi/anastudy/MagDown-11/DaVinci_Options_for_Ana.py' ) ] # The DaVinci options file you want to run
+j.name = "S21"+magPol+year+"data" 
+j.application.optsfile = [ File ( path+"DaVinci_Options.py" ) ]
 
-j.inputdata = j.application.readInputData("/afs/cern.ch/work/a/atrisovi/anastudy/MagDown-11/"+fileName) # The location of the data option file
+j.inputdata = j.application.readInputData(path + fileName) 
 
 j.splitter =  SplitByFiles ( filesPerJob = 30, maxFiles = -1 ) # This is the splitter - it controls how many data files are run over by a single subjob At the moment it's setup as one file per subjob - but this will result in a large number of subjobs, so you may want to increase the number.
-#1, -1
-#for testing 5, 100
-j.merger = None # Dont worry about this"Stripping21_D2HMuMu"+year+"_Mag"+magPol+"_NTuples.root
-j.outputfiles = ["Stripping21r1_D2HMuMu"+year+"_Mag"+magPol+"_NTuples.root"] # This is the name of the output root file. Make sure it matches what you specify in the DaVinci options, otherwise you wont be able to get hold of the output file!
-j.backend    = Dirac() # Leave this
-j.backend.settings['CPUTime'] = 172800 # Leave this
+
+j.merger = None 
+j.outputfiles = ["Stripping21r1_D2HMuMu"+year+"_Mag"+magPol+"_NTuples.root"] 
+j.backend    = Dirac() 
+j.backend.settings['CPUTime'] = 172800 
 j.submit()

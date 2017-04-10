8456bf8cd95d6b7d818ac969ce10d8c841703dcf
Ana Trisovic <ana.trisovic@gmail.com>
2017-04-10 17:14:57 +0200

    Analysis preservation hackathon #2

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-root -b Step4_tmva/TMVAClassification1.C\(\"/eos/lhcb/user/a/atrisovi/analysis/Analysis/Step3_MC\",\"/eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts\"\)
+# Exit if any command returns a non-zero exit code
+set -e
+
+root -q -b Step4_tmva/TMVAClassification1.C\(\"/eos/lhcb/user/a/atrisovi/analysis/Analysis/Step3_MC\",\"Step3_cuts\"\)
+
+root -q -b Step4_tmva/addBDT1.C\(\"Step3_cuts\"\) 
 
 echo "Hello world!"
 

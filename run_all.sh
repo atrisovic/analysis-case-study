1b5be2f47e7fa4394e3ca4ae1c3b4d8e0d951589
Ana Trisovic <ana.trisovic@gmail.com>
2017-04-10 16:11:25 +0200

    Analysis preservation hackathon #1

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+root -b Step4_tmva/TMVAClassification1.C\(\"/eos/lhcb/user/a/atrisovi/analysis/Analysis/Step3_MC\",\"/eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts\"\)
+
+echo "Hello world!"
+

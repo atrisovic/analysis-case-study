af64767337d7438897040c53857131e4f9a44a74
Ana Trisovic <ana.trisovic@gmail.com>
2017-05-11 16:17:50 +0200

    PimumuOS updated and new README

--- Step5_finalSelection/MonteCarlo/MCD2PimumuOS_class.C
+++ Step5_finalSelection/MonteCarlo/MCD2PimumuOS_class.C
@@ -1,14 +1,14 @@
-#define D2PimumuOS_class_cxx
-#include "D2PimumuOS_class.h"
+#define MCD2PimumuOS_class_cxx
+#include "MCD2PimumuOS_class.h"
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
 
-void D2PimumuOS_class::Loop()
+void MCD2PimumuOS_class::Loop()
 {
 //   In a ROOT session, you can do:
-//      root> .L D2PimumuOS_class.C
-//      root> D2PimumuOS_class t
+//      root> .L MCD2PimumuOS_class.C
+//      root> MCD2PimumuOS_class t
 //      root> t.GetEntry(12); // Fill t data members with entry number 12
 //      root> t.Show();       // Show values of entry 12
 //      root> t.Show(16);     // Read and show values of entry 16
@@ -29,38 +29,15 @@ void D2PimumuOS_class::Loop()
 // METHOD2: replace line
 //    fChain->GetEntry(jentry);       //read all branches
 //by  b_branchname->GetEntry(ientry); //read only this branch
-   
-	if (fChain == 0) {std::cout<<"We didnt find a tree, sorry!"<<std::endl; 
-return;}
+   if (fChain == 0) return;
 
-   TFile *const outFile1 = TFile::Open("D2PimumuOS_final.root", "recreate");
-	std::cout<<"We created empty file with name: "<<outFile1->GetName()<<std::endl; 
-   TTree *const outTree1 = fChain->CloneTree(0);
    Long64_t nentries = fChain->GetEntriesFast();
-	std::cout<<"[INFO] Run over "<<nentries<<" entries"<<std::endl; 
+
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       // if (Cut(ientry) < 0) continue;
-      // BDT cut > 0.1
-      if(BDT < 0.1) continue;
-      // PID mu > 1
-      if(muplus_PIDmu < 1) continue;
-      if(muminus_PIDmu < 1) continue;
-      // from ANA note: DDL_{KPi}<0
-      if(piplus_PIDK > 0) continue;
-      if(piplus_PIDmu > 0) continue;
-      // muons must satisfy ISMUON
-      if(muplus_isMuon == 0) continue;
-      if(muminus_isMuon == 0) continue;
-     outTree1->Fill();
    }
-
-std::cout<<"[INFO] Number of events in the output tree: "<<outTree1->GetEntries()<<std::endl; 
-std::cout<<"[INFO] Efficiency: "<<double(outTree1->GetEntries())/(double(nentries))<<std::endl;  
-   outFile1->Write();
-   outFile1->Close();
-
 }
--- Step5_finalSelection/README.md
+++ Step5_finalSelection/README.md
@@ -4,14 +4,39 @@ Run `root`
 
 ```
 $ TFile *_file1 = TFile::Open("root://eoslhcb.cern.ch//eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2KMuMuSS.root")
-   >> (TFile *) 0x35cbe30
+(TFile *) 0x35cbe30
 $ _file1->ls()
-   >> TNetXNGFile**		root://eoslhcb.cern.ch//eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2KMuMuSS.root	
-   >> TNetXNGFile*		root://eoslhcb.cern.ch//eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2KMuMuSS.root	
-   >>  KEY: TDirectoryFile	D2KmumuSSTuple;1	D2KmumuSSTuple
+TNetXNGFile**		root://eoslhcb.cern.ch//eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2KMuMuSS.root	
+TNetXNGFile*		root://eoslhcb.cern.ch//eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2KMuMuSS.root	
+KEY: TDirectoryFile	D2KmumuSSTuple;1	D2KmumuSSTuple
 $ TTree* tr1 = (TTree*) _file1->Get("D2KmumuSSTuple/DecayTree")
-   >> (TTree *) 0x35f0fc0
+(TTree *) 0x35f0fc0
 $ tr1->MakeClass("D2KmumuSS_class")
-   >> Info in <TTreePlayer::MakeClass>: Files: D2KmumuSS_class.h and D2KmumuSS_class.C generated from TTree: DecayTree
+Info in <TTreePlayer::MakeClass>: Files: D2KmumuSS_class.h and D2KmumuSS_class.C generated from TTree: DecayTree
 ```
 
+# Run ROOT scripts
+
+First: `lb-run ROOT csh`
+
+And then:
+
+```
+root [0] .L D2PimumuOS_class.C
+root [1] D2PimumuOS_class t
+file opened: /eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2PiMuMuOS.root
+TFile**     /eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2PiMuMuOS.root    
+TFile*     /eos/lhcb/user/a/atrisovi/analysis-case-study/Step3_cuts/D2PiMuMuOS.root    
+KEY: TDirectoryFile   D2PimumuOSTuple;1   D2PimumuOSTuple
+(D2PimumuOS_class &) @0x7f8610b9b000
+root [2] t.Loop()
+We created empty file with name: D2PimumuOS_final.root
+```
+
+Output:
+
+```
+[INFO] Run over 14394006 entries
+[INFO] Number of events in the output tree: 225818
+[INFO] Efficiency: 0.0156883
+```

9a989ad463bd224f84c6a895d7430559433bbbfa
Ana Trisovic <ana.trisovic@gmail.com>
2017-05-23 12:00:47 +0200

    Scripts that work

--- Step2_getMonteCarlo/MagUp-12/Ganga_Options.py
+++ Step2_getMonteCarlo/MagUp-12/Ganga_Options.py
@@ -3,22 +3,16 @@ myApplication.platform           = "x86_64-slc6-gcc48-opt"
 myApplication = DaVinci(version='v37r1p2') 					
 #myApplication.user_release_area  = "/eos/lhcb/user/a/atrisovi/analysis-case-study/Step2_getMonteCarlo/MagUp-12"		
 
-myApplication.directory = "./DaVinci_v37r1p2"
+#myApplication.directory = "."
 
-magPol = "Down" 
-year = "11"
+magPol = "Up" 
+year = "12"
 
-fileName = ""
-
-if magPol == "Down":
-    if year == "12":
-        fileName = "MC201221113000Beam4000GeV-2012-MagDown-Nu25-Pythia8Sim08aDigi13Trig0x409f0045Reco14Stripping20NoPrescalingFlaggedSTREAMSDST.py"
-    if year == "11":
-        fileName = "MC201121113000Beam3500GeV-2011-MagDown-Nu2-Pythia8Sim08aDigi13Trig0x40760037Reco14aStripping20r1NoPrescalingFlaggedSTREAMSDST.py"
+fileName = "MC201221113000Beam4000GeV-2012-MagUp-Nu25-Pythia8Sim08aDigi13Trig0x409f0045Reco14Stripping20NoPrescalingFlaggedSTREAMSDST.py"
 
 j = Job( application = myApplication )
 j.name = "MC"+magPol+year+"MS" 
-j.application.options = [ 'DaVinci_Options.py' ]
+j.application.optsfile = [ 'DaVinci_Options.py' ]
 j.inputdata = j.application.readInputData(fileName) 
 j.splitter =  SplitByFiles ( filesPerJob = 30, maxFiles = -1 ) 
 j.outputfiles = ["MC_D2PiMuMu"+year+"_Mag"+magPol+"_NTuples.root"] 

adec8417954686d6eb0643416d3bf25e362053d7
Ana Trisovic <ana.trisovic@gmail.com>
2017-07-17 17:05:04 +0200

    Added D2PiPiPi.

---
+++ Step2_getMonteCarlo/D2PiPiPi/MC201221263022Beam4000GeV-2012-MagDown-Nu25-Pythia8Sim08aDigi13Trig0x409f0045Reco14aStripping20FilteredD2HHHHLTSTRIPDST.py
@@ -0,0 +1,24 @@
+#-- GAUDI jobOptions generated on Mon Jul 17 14:38:38 2017
+#-- Contains event types : 
+#--   21263022 - 1 files - 22751 events - 5.81 GBytes
+
+
+#--  Extra information about the data processing phases:
+
+
+#--  Processing Pass Step-125363 
+
+#--  StepId : 125363 
+#--  StepName : Merge14 for Sim08 2012 Charm Filtered Productions (Amato)  
+#--  ApplicationName : DaVinci 
+#--  ApplicationVersion : v33r5 
+#--  OptionFiles : $APPCONFIGOPTS/Merging/DVMergeDST.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/Merging/WriteFSR.py;$APPCONFIGOPTS/Merging/MergeFSR.py 
+#--  DDDB : Sim08-20130503-1 
+#--  CONDDB : Sim08-20130503-1-vc-md100 
+#--  ExtraPackages : AppConfig.v3r164 
+#--  Visible : N 
+
+from Gaudi.Configuration import * 
+from GaudiConf import IOHelper
+IOHelper('ROOT').inputFiles(['LFN:/lhcb/MC/2012/D2HHH.HLTSTRIP.DST/00027820/0000/00027820_00000001_1.d2hhh.hltstrip.dst'
+], clear=True)

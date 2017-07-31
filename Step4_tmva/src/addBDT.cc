4c75a9935b0b622246d4cb5d32ef1200e0e2e1cc
jordix <none>
2017-07-31 16:34:13 +0200

    Fix creation of output ntuples.

--- Step4_tmva/src/addBDT.cc
+++ Step4_tmva/src/addBDT.cc
@@ -75,7 +75,7 @@ void addBDT( const std::string& datadir )
   Float_t BDT;     //BDTS classifier output
   Float_t var0,var1,var2,var3,var4,var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, spect0,spect1, spect2;
 
-  TFile out( "artifacts/D2PiMuMuOS.root", "w" );
+  TFile out( "artifacts/D2PiMuMuOS.root", "recreate" );
 
   TTree outtree( tname, dname + "/" + tname );
 

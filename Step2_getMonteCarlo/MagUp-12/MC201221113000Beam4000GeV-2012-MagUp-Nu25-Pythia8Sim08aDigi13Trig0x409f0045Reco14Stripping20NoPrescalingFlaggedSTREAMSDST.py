a7fa018eb8344a89daa1d081a8d0f273561e60a6
Ana Trisovic <ana.trisovic@gmail.com>
2017-09-28 13:04:31 +0200

    MC12UP

--- Step2_getMonteCarlo/MagUp-12/MC201221113000Beam4000GeV-2012-MagUp-Nu25-Pythia8Sim08aDigi13Trig0x409f0045Reco14Stripping20NoPrescalingFlaggedSTREAMSDST.py
+++ Step2_getMonteCarlo/MagUp-12/MC201221113000Beam4000GeV-2012-MagUp-Nu25-Pythia8Sim08aDigi13Trig0x409f0045Reco14Stripping20NoPrescalingFlaggedSTREAMSDST.py
@@ -1,74 +1,22 @@
-#-- GAUDI jobOptions generated on Wed Aug 12 15:34:02 2015
+#-- GAUDI jobOptions generated on Thu Sep 28 12:35:49 2017
 #-- Contains event types : 
-#--   21113000 - 120 files - 2054993 events - 435.57 GBytes
+#--   21113000 - 59 files - 1015494 events - 215.38 GBytes
 
 
 #--  Extra information about the data processing phases:
 
 
-#--  Processing Pass Step-124620 
+#--  Processing Pass Step-124629 
 
-#--  StepId : 124620 
-#--  StepName : Digi13 with G4 dE/dx 
-#--  ApplicationName : Boole 
-#--  ApplicationVersion : v26r3 
-#--  OptionFiles : $APPCONFIGOPTS/Boole/Default.py;$APPCONFIGOPTS/Boole/DataType-2012.py;$APPCONFIGOPTS/Boole/Boole-SiG4EnergyDeposit.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py 
-#--  DDDB : fromPreviousStep 
-#--  CONDDB : fromPreviousStep 
+#--  StepId : 124629 
+#--  StepName : Merge14 for Sim08 
+#--  ApplicationName : LHCb 
+#--  ApplicationVersion : v35r4 
+#--  OptionFiles : $APPCONFIGOPTS/Merging/CopyDST.py 
+#--  DDDB : None 
+#--  CONDDB : None 
 #--  ExtraPackages : AppConfig.v3r164 
-#--  Visible : Y 
-
-
-#--  Processing Pass Step-124594 
-
-#--  StepId : 124594 
-#--  StepName : Reco14 for MC 
-#--  ApplicationName : Brunel 
-#--  ApplicationVersion : v43r2p6 
-#--  OptionFiles : $APPCONFIGOPTS/Brunel/DataType-2012.py;$APPCONFIGOPTS/Brunel/MC-WithTruth.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py 
-#--  DDDB : fromPreviousStep 
-#--  CONDDB : fromPreviousStep 
-#--  ExtraPackages : AppConfig.v3r164 
-#--  Visible : Y 
-
-
-#--  Processing Pass Step-124632 
-
-#--  StepId : 124632 
-#--  StepName : TCK-0x409f0045 Flagged for Sim08 2012 
-#--  ApplicationName : Moore 
-#--  ApplicationVersion : v14r8p1 
-#--  OptionFiles : $APPCONFIGOPTS/Moore/MooreSimProductionWithL0Emulation.py;$APPCONFIGOPTS/Conditions/TCK-0x409f0045.py;$APPCONFIGOPTS/Moore/DataType-2012.py;$APPCONFIGOPTS/L0/L0TCK-0x0045.py 
-#--  DDDB : fromPreviousStep 
-#--  CONDDB : fromPreviousStep 
-#--  ExtraPackages : AppConfig.v3r164 
-#--  Visible : Y 
-
-
-#--  Processing Pass Step-124630 
-
-#--  StepId : 124630 
-#--  StepName : Stripping20-NoPrescalingFlagged for Sim08 
-#--  ApplicationName : DaVinci 
-#--  ApplicationVersion : v32r2p1 
-#--  OptionFiles : $APPCONFIGOPTS/DaVinci/DV-Stripping20-Stripping-MC-NoPrescaling.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/InputType-DST.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py 
-#--  DDDB : fromPreviousStep 
-#--  CONDDB : fromPreviousStep 
-#--  ExtraPackages : AppConfig.v3r164 
-#--  Visible : Y 
-
-
-#--  Processing Pass Step-124817 
-
-#--  StepId : 124817 
-#--  StepName : Sim08a - 2012 - MU - Pythia8 
-#--  ApplicationName : Gauss 
-#--  ApplicationVersion : v45r2 
-#--  OptionFiles : $APPCONFIGOPTS/Gauss/Sim08-Beam4000GeV-mu100-2012-nu2.5.py;$DECFILESROOT/options/@{eventType}.py;$LBPYTHIA8ROOT/options/Pythia8.py;$APPCONFIGOPTS/Gauss/G4PL_FTFP_BERT_EmNoCuts.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py 
-#--  DDDB : Sim08-20130503-1 
-#--  CONDDB : Sim08-20130503-1-vc-mu100 
-#--  ExtraPackages : AppConfig.v3r169;DecFiles.v27r4 
-#--  Visible : Y 
+#--  Visible : N 
 
 from Gaudi.Configuration import * 
 from GaudiConf import IOHelper
@@ -130,66 +78,5 @@ IOHelper('ROOT').inputFiles(['LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/000
 'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/00024296_00000059_1.allstreams.dst',
 'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/00024296_00000060_1.allstreams.dst',
 'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/00024296_00000061_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/00024296_00000062_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000001_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000004_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000005_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000006_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000007_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000008_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000009_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000010_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000011_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000012_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000013_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000014_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000015_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000016_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000017_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000018_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000019_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000020_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000021_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000022_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000023_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000024_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000025_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000026_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000027_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000028_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000029_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000030_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000031_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000032_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000033_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000034_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000035_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000036_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000037_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000038_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000039_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000040_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000041_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000042_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000043_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000044_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000045_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000046_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000047_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000048_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000049_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000050_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000051_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000052_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000054_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000055_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000056_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000057_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000058_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000059_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000060_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000061_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000062_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000063_1.allstreams.dst',
-'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024298/0000/00024298_00000064_1.allstreams.dst'
+'LFN:/lhcb/MC/2012/ALLSTREAMS.DST/00024296/0000/00024296_00000062_1.allstreams.dst'
 ], clear=True)
